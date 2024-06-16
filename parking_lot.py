28a15661d8c423bc330452ed5f4ee50d073b67d3
Anny <149850750+Potato-chipz@users.noreply.github.com>
2024-06-16 19:49:36 +0800

    Update parking_lot.py

--- parking_lot.py
+++ parking_lot.py
@@ -1,19 +1,29 @@
 """To scan a car when enters the parking lot, store the plate number in a list. When exits can put in the plate number on a person's phone, and if matches, can pay the pakring lot fee """
 
+open = True # parking lot is open 
 car_in = [] 
-"Scan the car that comes into the parking lot"
-car = input("Car coming in! Enter the plate number: ")
-car_in.append(car)
 
-flag = True #Re-enter plate number
+while open:
+    # 1 for car coming in, 0 for car coming out 
+    option = input("Please select the option, 1 for IN, 0 for OUT: ")
 
-while (flag): 
-    "Enter the plate number when exits the parking lot"
-    car_out = input("Car going out! Enter the plate number: ")
+    if option == '1': 
+        
+        "Scan the car that comes into the parking lot"
+        car = input("Car coming in! Enter the plate number: ")
+        car_in.append(car)
 
-    for i in car_in: 
-        if i == car_out: 
-            print("Payment: $10")
-            flag = False
-        else: 
-            print("Please re-enter the plate number")
+    elif option == '0': 
+        flag = 1
+        while flag:
+            "Enter the plate number in parking lot"
+            car_out = input("Car going out! Enter the plate number: ")
+
+            for i in car_in: 
+                if i == car_out: 
+                    print("Payment: $10")
+                    car_in.remove(car_out) #remove the car in the park
+                    print(f'Car still in PL: {car_in}')
+                    flag = 0
+                else: 
+                    print("Please re-enter")
